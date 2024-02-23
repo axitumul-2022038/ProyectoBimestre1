78d388b440f4b4c0581cfc0740791ec7f0f71695
axitumul-2022038 <axitumul-2022038@kinal.edu.gt>
2024-02-23 11:42:40 -0600

    quinto commit se agrego validaciones a product y categories al igual que las colecciones para cada entidad

--- src/categories/categories.routes.js
+++ src/categories/categories.routes.js
@@ -1,15 +1,16 @@
 'use strict'
 
 import { Router } from "express"
+import { isAdmin, validateJwt } from "../../middlewares/validate-jwt.js"
 import { register,test,get,update,deleteC, search } from "./categories.controller.js"
 
 const api = Router()
 
 api.get('/test', test)
-api.post('/register', register)
+api.post('/register',[validateJwt, isAdmin], register)
 api.get('/get', get)
-api.put('/update/:id', update)
-api.delete('/delete/:id', deleteC)
-api.post('/search', search)
+api.put('/update/:id',[validateJwt, isAdmin], update)
+api.delete('/delete/:id',[validateJwt, isAdmin], deleteC)
+api.post('/search',[validateJwt, isAdmin], search)
 
 export default api
--- src/products/products.routes.js
+++ src/products/products.routes.js
@@ -1,15 +1,15 @@
 'use strict'
-
+import { isAdmin,validateJwt } from "../../middlewares/validate-jwt.js"
 import { Router } from "express"
 import { test, register, get, update, deleteP, search } from "./products.controller.js"
 
 const api = Router()
 
 api.get('/test', test)
-api.post('/register', register)
-api.get('/get', get)
-api.put('/update/:id', update)
-api.delete('/delete/:id', deleteP)
-api.post('/search', search)
+api.post('/register',[validateJwt, isAdmin], register)
+api.get('/get',[validateJwt, isAdmin], get)
+api.put('/update/:id',[validateJwt, isAdmin], update)
+api.delete('/delete/:id',[validateJwt, isAdmin], deleteP)
+api.post('/search',[validateJwt, isAdmin], search)
 
 export default api
